8fe8742fc1578f18e3b92c27d6543858625e9c4f
Vendula Krajíčková <vendysacek@seznam.cz>
2024-04-20 15:38:34 +0200

    #2 - set useState name from Form

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,11 @@ interface JokeDataStructure {
 function App() {
 
   const [jokeData, setJokeData] = useState<JokeDataStructure[]>([]);
+  const [name, setName] = useState<string>('');
 
   const handleSubmit = (data: FormDataStructure) => {
-    console.log(data.name);
+    console.log(data);
+    setName(data.name);
   }
 
   return (
@@ -30,7 +32,7 @@ function App() {
             </>
           ) : (
             <>
-              <h2 className="title">Your name</h2>
+              <h2 className="title">{name}</h2>
               <h3 className="subtitle">There are jokes for you!</h3>
               <Joke setup="What did the judge say to the dentist?" punchline="Do you swear to pull the tooth, the whole tooth and nothing but the tooth?" />
               <span className="app__container__back-icon"><HiMiniArrowUturnLeft className="icon-arrowBack" /></span>
